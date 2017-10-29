0825d4bb6b0642c28906c4d63ddadd2d9812aa17
Avnerus <avnerus@gmail.com>
2017-10-29 15:34:07 +0200

    change ms translator token method. fixes #15

--- src/server/util/mstranslate.js
+++ src/server/util/mstranslate.js
@@ -4,7 +4,15 @@ class MSTranslateUtil {
     constructor() {
         this.client = new MSTranslator({
             api_key : process.env.MS_ACCESS_KEY
-        },true)
+        },false) // No auto refresh, token will be initialzied and refreshed at interval
+
+        this.initToken()
+        .then((keys) => {
+            console.log("M$ KEYS: ", keys);
+        })
+        .catch((err) => {
+            console.error("Error renewing MSTranslator keys!", err);
+        });
     }
     translate(text, target) {
         return new Promise((resolve, reject) => {
@@ -17,6 +25,18 @@ class MSTranslateUtil {
                   reject(err);
               } else {
                   resolve(data);
+              }
+            });
+        })
+    }
+
+    initToken() {
+        return new Promise((resolve, reject) => {
+            this.client.initialize_token((err, keys) => {
+              if (err) {
+                  reject(err);
+              } else {
+                  resolve(keys);
               }
             });
         })
